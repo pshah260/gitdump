f9f1e6dad5f927e08f37bf2054a8dc48776370dc
pshah260 <pranay.shah@outlook.com>
2020-10-05 23:36:20 -0400

    update

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
 class api:
     def __init__(self):
-        self.secret = "3X5m3FXm2WANtyYsHheR5vpMzoZU"
-        self.key = "account-UcjAJrtjvZ0uYr1PdJha"
+        self.secret = ""
+        self.key = ""
